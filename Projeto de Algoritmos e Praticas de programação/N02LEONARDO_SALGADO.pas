5a6fea157ad21510bca8c1fc14706545802de6c6
Leonardo Felipe Salgado <53799801+Salgado2004@users.noreply.github.com>
2023-06-04 23:07:09 -0300

    Update N02LEONARDO_SALGADO.pas

--- Projeto de Algoritmos e Praticas de programação/N02LEONARDO_SALGADO.pas
+++ Projeto de Algoritmos e Praticas de programação/N02LEONARDO_SALGADO.pas
@@ -5,39 +5,39 @@ Program N02LEONARDO_SALGADO;
 mostre o próprio número e a relação de seus divisores.}
 
 var num, result: integer;
-		loop: string[3] = 'sim';
+    loop: string[3] = 'sim';
 		
 procedure getDivisores(num:integer);
-	var divs: integer;
-	begin
-		divs := num;
-		if (divs < 0) then
-			divs := divs * -1;
-		writeln('Divisores: ');
-		repeat
-			result := num mod divs;
-			if (result = 0) then
-				writeln('		', divs);
-			divs := divs-1;
-		until divs = 0;
-	end;
+    var divs: integer;
+     begin
+      divs := num;
+      if (divs < 0) then
+	divs := divs * -1;
+	writeln('Divisores: ');
+	repeat
+	 result := num mod divs;
+	 if (result = 0) then
+	 writeln('		', divs);
+	 divs := divs-1;
+	until divs = 0;
+     end;
 
 Begin
-	writeln('Trabalho 2: Um número e seus divisores');
-	while (loop = 'sim') do
-		begin
-			writeln();
-			writeln('Digite um número inteiro e pressione Enter: ');
-			readln(num);   
-			if (num = 0) then 
-				begin
-			  	write('ERRO: 0 não possui divisores');
-					break;
-				end;
-			writeln('Número lido: ', num);
-			getDivisores(num);
-			writeln();
-			writeln('Deseja continuar? sim/não');
-			readln(loop);
-		end;
+ writeln('Trabalho 2: Um número e seus divisores');
+ while (loop = 'sim') do
+  begin
+   writeln();
+   writeln('Digite um número inteiro e pressione Enter: ');
+   readln(num);   
+   if (num = 0) then 
+    begin
+     write('ERRO: 0 não possui divisores');
+     break;
+    end;
+   writeln('Número lido: ', num);
+   getDivisores(num);
+   writeln();
+   writeln('Deseja continuar? sim/não');
+   readln(loop);
+  end;
 End.
