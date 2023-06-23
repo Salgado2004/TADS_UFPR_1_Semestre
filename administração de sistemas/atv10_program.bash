de2b96cc5f06f1adadb4aed478ec9d8062b963b9
Leonardo Felipe Salgado <53799801+Salgado2004@users.noreply.github.com>
2023-06-23 15:03:34 -0300

    Create atv10_program.bash

---
+++ administração de sistemas/atv10_program.bash
@@ -0,0 +1,5 @@
+#Não é de fato um programa funcional, apenas para me lembrar da estrutura do comando
+#Feito por Leonardo Felipe Salgado
+
+crontab -e
+*/1 * * * * Home/salgado/Downloads/teste_cron.bash
