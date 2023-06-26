c3c633a289f15ecc0832f9535d2020bab744f033
Leonardo Felipe Salgado <53799801+Salgado2004@users.noreply.github.com>
2023-06-26 14:59:11 -0300

    Update projetoFinal_backups.bash

--- administração de sistemas/projetoFinal_backups.bash
+++ administração de sistemas/projetoFinal_backups.bash
@@ -9,14 +9,25 @@ echo
 locali=$1
 user=$2
 echo "Local: $locali"
-pasta=(`ls $locali`)
-echo "Arquivos: ${pasta[@]}"
+pasta=(`ls $locali`) #Recebe os arquivos da pasta atual
+
+echo -n "Arquivos: "
+for file in ${pasta[@]}; do #Mostra os arquivos um por um
+ typeF=(`file $locali/$file`)
+ if [ ${typeF[1]} = "directory" ]; then #Formata em negrito as pastas
+  echo -n -e "\033[01;32m$file\033[00;37m "
+ else
+  echo -n "$file " #Formata normal os arquivos
+ fi
+done
+
+echo #Mostra opções para o usuário
 echo "O que você deseja fazer?"
 echo "1 - Selecionar arquivos para backup"
 echo "2 - Navegar entre as pastas"
 echo "3 - sair"
 read action
-case $action in
+case $action in #Vê a opção escolhida
  1) echo "Você escolheu 1";;
  2) echo "Você escolheu 2";;
  3) exit;;
@@ -25,11 +36,11 @@ esac
 }
 
 Principal(){
-clear
+clear #Inicio do programa
 echo "Projeto Final - Sistema de Backup de Arquivos"
-user=`id -un`
-locali="/home/$user"
-Menu $locali $user
+user=`id -un` #Lê o usuário utilizando o programa
+locali="/home/$user" #Posiciona o leitor de arquivos na pasta do usuário
+Menu $locali $user #Chama a função menu
 }
 
 Principal
