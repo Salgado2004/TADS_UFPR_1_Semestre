9fe9a81d444d09507bdea8b7063efbb98119e0d8
Leonardo Felipe Salgado <53799801+Salgado2004@users.noreply.github.com>
2023-06-27 11:06:29 -0300

    Update projetoFinal_backups.bash

--- administração de sistemas/projetoFinal_backups.bash
+++ administração de sistemas/projetoFinal_backups.bash
@@ -4,70 +4,104 @@
 #Leonardo Felipe Salgado
 #Raul Ferreira Bana
 
-Navegar(){ #Permite ao usuário navegar e visualizar as pastas e arquivos
-locali=$1
-user=$2
-file=($@)
-echo "Navegar entre as pastas: " #Explica como funciona
-echo "Selecione o índice da pasta que deseja entrar"
-echo "Ou digite 00 para voltar"
-read nav
-if [ $nav = "00" ]; then #Se ele desejar voltar, o sistema modifica sozinho o local
- nav=(${locali//"/"/ })
- lenght=${#nav[@]}
- echo $lenght
- locali=""
- for ((i=0;i<$lenght-1;i++)); do
- locali=$locali/${nav[i]}
+Selecionar_origem(){ #Permite ao usuário selecionar os arquivos de origem 
+ files=($@)
+ echo
+ echo "Você escolheu selecionar origem"
+ echo "Digite os índices dos arquivos e/ou pastas desejados, separados por espaço ' ', e aperte ENTER para confirmar"
+ read indices #Lê os arquivos
+ indices=($indices) #Transforma em vetor
+ for indice in ${indices[@]}; do
+  origem=(${origem[@]} ${files[indice]})
  done
-else #Se ele desejar avançar, o sistema busca a pasta escolhida pelo indice e atualiza o local
- nav=$((nav + 2))
- locali=$locali"/"${file[nav]}
-fi
-Menu $locali $2 #Chama o menu novamente
+ Menu $locali $user #Chama o menu novamente
 }
 
-Menu(){
-clear
-echo
-locali=$1
-user=$2
-echo "Local: $locali"
-pasta=(`ls $locali`) #Recebe os arquivos da pasta atual
-
-cont=0 #Cria um contador de índices
-echo -n "Arquivos: "
-for file in ${pasta[@]}; do #Mostra os arquivos um por um
- typeF=(`file $locali/$file`)
- case ${typeF[1]} in
- "directory")  echo -n -e "\033[01;32m$file\033[00;37m($cont)  ";; #Formata em negrito as pastas
- "Bourne-Again") echo -n -e "\033[01;31m$file\033[00;37m($cont)  ";;
- "Unicode") echo -n -e "\033[00;34m$file\033[00;37m($cont)  ";;
- *) echo -n "$file($cont)  ";; #Formata normal os arquivos
+Selecionar_arquivos(){ #Permite ao usuário selecionar os arquivos de origem e/ou destino
+ files=($@)
+ echo "Selecionar arquivos para backup: " #Explica como funciona
+ echo "1 - Selecionar arquivos de origem"
+ echo "2 - Selecionar pasta de destino"
+ read opt
+ case $opt in
+  1) Selecionar_origem ${files[@]};;
+  2) Selecionar_destino ${files[@]};;
+  *) Selecionar_arquivos ${files[@]};;
  esac
- cont=$((cont + 1))
-done
+}
 
-echo #Mostra opções para o usuário
-echo "O que você deseja fazer?"
-echo "1 - Selecionar arquivos para backup"
-echo "2 - Navegar entre as pastas"
-echo "3 - sair"
-read action
-case $action in #Vê a opção escolhida
- 1) echo "Você escolheu 1";;
- 2) Navegar $1 $2 ${pasta[@]};;
- 3) exit;;
- *) Menu $1 $2;; 
-esac
+Navegar(){ #Permite ao usuário navegar e visualizar as pastas e arquivos
+ file=($@)
+ echo "Navegar entre as pastas: " #Explica como funciona
+ echo "Selecione o índice da pasta que deseja entrar"
+ echo "Ou digite 00 para voltar"
+ read nav
+ 
+ if [ $nav = "00" ]; then #Se ele desejar voltar, o sistema modifica sozinho o caminho
+  nav=(${locali//"/"/ })
+  lenght=${#nav[@]}
+  echo $lenght
+  locali=""
+  for ((i=0;i<$lenght-1;i++)); do
+   locali=$locali/${nav[i]}
+  done
+ else #Se ele desejar avançar, o sistema busca a pasta escolhida pelo indice e atualiza o caminho
+  typeF=(`file $locali"/"${file[nav]}`)
+  if [ ${typeF[1]} = "directory" ]; then #Verifica se o caminho escolhido é uma pasta 
+   locali=$locali"/"${file[nav]}
+  else
+   echo "O arquivo escolhido não é um diretório!" #Se não for, não atualiza o caminho
+   echo "Aperte ENTER para continuar"
+   read enter
+  fi
+ fi
+ Menu $locali $user #Chama o menu novamente
+}
+
+Menu(){ #Permite ao usuário interagir com o sistema
+ clear
+ echo "Projeto Final - Sistema de Backup de Arquivos"
+ echo
+ locali=$1
+ user=$2
+ echo "Local: $locali"
+ pasta=(`ls $locali`) #Recebe os arquivos da pasta atual
+
+ cont=0 #Cria um contador de índices
+ echo -n "Arquivos: "
+ for file in ${pasta[@]}; do #Mostra os arquivos um por um
+  typeF=(`file $locali/$file`)
+  case ${typeF[1]} in
+   "directory")  echo -n -e "\033[01;32m$file\033[00;37m($cont)   ";; #Formata em verde e  negrito as pastas
+   "Bourne-Again") echo -n -e "\033[01;31m$file\033[00;37m($cont)   ";; #Formata em vermelho os executaveis
+   "Unicode") echo -n -e "\033[00;34m$file\033[00;37m($cont)   ";; #Formata em azul os .txt
+   *) echo -n "$file($cont)  ";; #Formata normal os arquivos
+  esac
+  cont=$((cont + 1))
+ done
+ echo 
+ echo "Arquivos selecionados: ${#origem[@]}"
+ echo #Mostra opções para o usuário
+ echo "O que você deseja fazer?"
+ echo "1 - Selecionar arquivos para backup"
+ echo "2 - Criar pasta para backup"
+ echo "3 - Navegar entre as pastas"
+ echo "4 - sair"
+ read action
+ case $action in #Vê a opção escolhida
+  1) Selecionar_arquivos ${pasta[@]};;
+  2) echo "Você escolheu 2";;
+  3) Navegar ${pasta[@]};;
+  4) exit;;
+  *) Menu $1 $2;; 
+ esac
 }
 
 Principal(){
-clear #Inicio do programa
-echo "Projeto Final - Sistema de Backup de Arquivos"
-user=`id -un` #Lê o usuário utilizando o programa
-locali="/home/$user" #Posiciona o leitor de arquivos na pasta do usuário
-Menu $locali $user #Chama a função menu
+ #Inicio do programa
+ user=`id -un` #Lê o usuário utilizando o programa
+ locali="/home/$user" #Posiciona o leitor de arquivos na pasta do usuário
+ Menu $locali $user #Chama a função menu
 }
 
 Principal
