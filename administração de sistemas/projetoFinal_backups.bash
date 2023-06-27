d76af47f69bd3c2cd08c035212d883bf936341bc
Leonardo Felipe Salgado <53799801+Salgado2004@users.noreply.github.com>
2023-06-27 15:00:46 -0300

    Update projetoFinal_backups.bash

--- administração de sistemas/projetoFinal_backups.bash
+++ administração de sistemas/projetoFinal_backups.bash
@@ -4,6 +4,56 @@
 #Leonardo Felipe Salgado
 #Raul Ferreira Bana
 
+Realizar_backup(){
+ clear
+ echo -e "\033[01;37mRealizar backup\033[00;37m"
+ echo
+ echo "Arquivos para backup: ${origem[@]}"
+ echo "Pasta de destino: $destino"
+ for file in ${origem[@]}; do
+  rsync -avz --progress $file $destino
+ done
+ echo
+ echo -e "\033[05mBackup realizado!\033[00m"
+ read enter
+ Menu $locali $user
+}
+
+Criar_pasta(){ #Permite ao usuário criar uma pasta de destino
+ echo "Criar pasta: " 
+ echo "Digite o nome que você deseja dar para a pasta"
+ read nome
+ echo `mkdir $locali/$nome`
+ destino="$locali/$nome"
+ if [ ${#origem[@]} -gt 0 ]; then #Se os arquivos de origem já tiverem sido selecionados, chama a função de backup
+  Realizar_backup
+ else
+  Menu $locali $user #Chama o menu novamente
+ fi
+}
+
+Selecionar_destino(){ #Permite ao usuário selecionar a pasta de destino
+ files=($@)
+ echo
+ echo "Você escolheu selecionar destino"
+ echo "Digite o índice da pasta desejada e aperte ENTER para confirmar"
+ read indice #Lê a pasta
+ typeF=(`file $locali"/"${files[indice]}`)
+ if [ ${typeF[1]} = "directory" ]; then #Verifica se o indice escolhido é uma pasta 
+  destino="$locali/${files[indice]}"
+  if [ ${#origem[@]} -gt 0 ]; then #Se os arquivos de origem já tiverem sido selecionados, chama a função de backup
+   Realizar_backup
+  else
+   Menu $locali $user #Chama o menu novamente
+  fi
+ else
+  echo "O arquivo escolhido não é um diretório!" #Se não for, não seleciona
+  echo "Aperte ENTER para continuar"
+  read enter
+  Menu $locali $user #Chama o menu novamente
+ fi 
+}
+
 Selecionar_origem(){ #Permite ao usuário selecionar os arquivos de origem 
  files=($@)
  echo
@@ -12,9 +62,13 @@ Selecionar_origem(){ #Permite ao usuário selecionar os arquivos de origem
  read indices #Lê os arquivos
  indices=($indices) #Transforma em vetor
  for indice in ${indices[@]}; do
-  origem=(${origem[@]} ${files[indice]})
+  origem=(${origem[@]} "$locali/${files[indice]}")
  done
- Menu $locali $user #Chama o menu novamente
+ if [ ${#destino[@]} -gt 0 ]; then
+  Realizar_backup
+ else
+  Menu $locali $user #Chama o menu novamente
+ fi
 }
 
 Selecionar_arquivos(){ #Permite ao usuário selecionar os arquivos de origem e/ou destino
@@ -81,6 +135,7 @@ Menu(){ #Permite ao usuário interagir com o sistema
  done
  echo 
  echo "Arquivos selecionados: ${#origem[@]}"
+ echo "Local para o backup: $destino"
  echo #Mostra opções para o usuário
  echo "O que você deseja fazer?"
  echo "1 - Selecionar arquivos para backup"
@@ -90,7 +145,7 @@ Menu(){ #Permite ao usuário interagir com o sistema
  read action
  case $action in #Vê a opção escolhida
   1) Selecionar_arquivos ${pasta[@]};;
-  2) echo "Você escolheu 2";;
+  2) Criar_pasta;;
   3) Navegar ${pasta[@]};;
   4) exit;;
   *) Menu $1 $2;; 
